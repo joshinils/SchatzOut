bce5cc9c145ec872ea954cbb11a34462da6d686f
joshinils <joshua.laskowski@web.de>
2018-10-06 14:14:46 +0200

    wip

--- schatzOut/schatzOut/Brett.h
+++ schatzOut/schatzOut/Brett.h
@@ -42,7 +42,7 @@ public:
 			return InvalidPos;
 		return _brett[z][s];
 	}	// get value of position
-	
+
 	int get(std::pair<int, int> pos) const
 	{
 		if (pos.first >= squareSize || pos.second >= squareSize)
@@ -55,22 +55,21 @@ public:
 		if (   (_schatzPosition.first == 0 && _schatzPosition.second == 1)
 			|| (_schatzPosition.first == 1 && _schatzPosition.second == 0) )
 		{
-			_brett[0][0] = Neighboring;
 			_brett[0][1] = PossibleTreasure;
 			_brett[1][0] = PossibleTreasure;
-			return Neighboring;
+			return _brett[0][0] = Neighboring;
 		}
 		return _brett[0][0] = Empty;
 	}
 
 	int revealCornerUpperRight()
 	{
-		if (   (_schatzPosition.first == 0 && _schatzPosition.second == 5)
-			|| (_schatzPosition.first == 1 && _schatzPosition.second == 4))
+		if (   (_schatzPosition.first == 0 && _schatzPosition.second == squareSize-1)
+			|| (_schatzPosition.first == 1 && _schatzPosition.second == squareSize-2))
 		{
-			_brett[0][5] = Neighboring;
-			_brett[0][5] = PossibleTreasure;
-			_brett[1][4] = PossibleTreasure;
+			_brett[0][squareSize-1] = Neighboring;
+			_brett[0][squareSize-1] = PossibleTreasure;
+			_brett[1][squareSize-2] = PossibleTreasure;
 			return Neighboring;
 		}
 		return _brett[0][5] = Empty;
@@ -78,131 +77,174 @@ public:
 
 	int revealCornerLowerLeft()
 	{
-		if (   (_schatzPosition.first == 4 && _schatzPosition.second == 0)
-			|| (_schatzPosition.first == 5 && _schatzPosition.second == 1) )
+		if (   (_schatzPosition.first == squareSize-2 && _schatzPosition.second == 0)
+			|| (_schatzPosition.first == squareSize-1  && _schatzPosition.second == 1) )
 		{
-			_brett[5][0] = Neighboring;
-			_brett[4][0] = PossibleTreasure;
-			_brett[5][1] = PossibleTreasure;
-			return Neighboring;
+			_brett[squareSize-2][0] = PossibleTreasure;
+			_brett[squareSize-1][1] = PossibleTreasure;
+			return _brett[squareSize-1][0] = Neighboring;
 		}
-		return _brett[5][0] = Empty;
+		return _brett[squareSize][0] = Empty;
 	}
 
 	int revealCornerLowerRight()
 	{
-		if (   (_schatzPosition.first == 5 && _schatzPosition.second == 4)
-			|| (_schatzPosition.first == 4 && _schatzPosition.second == 5) )
+		if (   (_schatzPosition.first == squareSize-1 && _schatzPosition.second == squareSize-2)
+			|| (_schatzPosition.first == squareSize-2 && _schatzPosition.second == squareSize-1) )
 		{
-			_brett[5][5] = Neighboring;
-			_brett[5][4] = PossibleTreasure;
-			_brett[4][5] = PossibleTreasure;
-			return Neighboring;
+			_brett[squareSize-1][squareSize-2] = PossibleTreasure;
+			_brett[squareSize-2][squareSize-1] = PossibleTreasure;
+			return _brett[squareSize-1][squareSize-1] = Neighboring;
 		}
-		return _brett[5][5] = Empty;
+		return _brett[squareSize][squareSize] = Empty;
+	}
+
+	int revealBorderTop(int spalte)
+	{
+		if (   (_schatzPosition.first == 0 && _schatzPosition.second == spalte - 1) // left
+			|| (_schatzPosition.first == 0 && _schatzPosition.second == spalte + 1) // right
+			|| (_schatzPosition.first == 1 && _schatzPosition.second == spalte    ) ) // below
+		{
+			_brett[0][spalte - 1] = PossibleTreasure;
+			_brett[0][spalte + 1] = PossibleTreasure;
+			_brett[1][spalte    ] = PossibleTreasure;
+			return _brett[0][spalte] = Neighboring;
+		}
+
+		return _brett[0][spalte] = Empty;
+	}
+
+	int revealBorderBottom(int spalte)
+	{
+		if (   (_schatzPosition.first == squareSize - 1 && _schatzPosition.second == spalte - 1) // left
+			|| (_schatzPosition.first == squareSize - 1 && _schatzPosition.second == spalte + 1) // right
+			|| (_schatzPosition.first == squareSize - 2 && _schatzPosition.second == spalte    ) ) // above
+		{
+			_brett[squareSize - 1][spalte - 1] = PossibleTreasure;
+			_brett[squareSize - 1][spalte + 1] = PossibleTreasure;
+			_brett[squareSize - 2][spalte    ] = PossibleTreasure;
+			return _brett[squareSize - 1][spalte    ] = Neighboring;
+		}
+
+		return _brett[squareSize -1][spalte] = Empty;
+	}
+
+	int revealBorderLeft(int zeile)
+	{
+		if (   (_schatzPosition.first == zeile - 1 && _schatzPosition.second == 0) // above
+			|| (_schatzPosition.first == zeile + 1 && _schatzPosition.second == 0) // below
+			|| (_schatzPosition.first == zeile     && _schatzPosition.second == 1) ) // right
+		{
+			_brett[zeile - 1][0] = PossibleTreasure;
+			_brett[zeile + 1][0] = PossibleTreasure;
+			_brett[zeile    ][1] = PossibleTreasure;
+			return _brett[zeile    ][0] = Neighboring;
+		}
+
+		return _brett[zeile][0] = Empty;
+	}
+
+	int revealBorderRight(int zeile)
+	{
+		if (   (_schatzPosition.first == zeile - 1 && _schatzPosition.second == squareSize - 1) // above
+			|| (_schatzPosition.first == zeile + 1 && _schatzPosition.second == squareSize - 1) // below
+			|| (_schatzPosition.first == zeile     && _schatzPosition.second == squareSize - 2) ) // left
+		{
+			_brett[zeile - 1][squareSize - 1] = PossibleTreasure;
+			_brett[zeile + 1][squareSize - 1] = PossibleTreasure;
+			_brett[zeile    ][squareSize - 2] = PossibleTreasure;
+			return _brett[zeile    ][squareSize - 1] = Neighboring;
+		}
+
+		return _brett[zeile][squareSize - 1] = Empty;
+	}
+
+	int revealMiddle(int zeile, int spalte)
+	{
+		if (   (_schatzPosition.first == zeile - 1 && _schatzPosition.second == spalte    ) // above
+		    || (_schatzPosition.first == zeile + 1 && _schatzPosition.second == spalte    ) // below
+		    || (_schatzPosition.first == zeile     && _schatzPosition.second == spalte - 1) // left
+		    || (_schatzPosition.first == zeile     && _schatzPosition.second == spalte + 1) ) // right
+		{
+			_brett[zeile - 1][spalte    ] = PossibleTreasure;
+			_brett[zeile + 1][spalte    ] = PossibleTreasure;
+			_brett[zeile    ][spalte - 1] = PossibleTreasure;
+			_brett[zeile    ][spalte + 1] = PossibleTreasure;
+			return _brett[zeile    ][spalte    ] = Neighboring;
+		}
+		return _brett[zeile][spalte] = Empty;
 	}
 
 	// change board state by revealing one position
 	int reveal(std::pair<int, int> pos)
 	{
+		// schatz at pos to be revealed
 		if (pos == _schatzPosition)
 			return _brett[pos.first][pos.second] = Treasure;
+
 		if (pos.first >= squareSize || pos.second >= squareSize)
 			return InvalidPos;
+
 		switch (pos.first)
 		{
-		case 0:
-			switch (pos.second)
-			{
-			case 0:
-				return revealCornerUpperLeft();
-				break;
+			case 0: // top
+				switch (pos.second)
+				{
+					case 0:  return revealCornerUpperLeft();
+					case 1:
+					case 2:
+					case 3:
+					case 4:  return revealBorderTop(pos.second);
+					case 5:  return revealCornerUpperRight();
+					default: return InvalidPos;
+				}
 			case 1:
 			case 2:
 			case 3:
-			case 4:
-				//upper middle
-				break;
-
-			case 5:
-				return revealCornerUpperRight();
-				break;
-
-			default:
-				return InvalidPos;
-				break;
-			}
-			break;
-
-		case 1:
-		case 2:
-		case 3:
-		case 4:
-			switch (pos.second)
-			{
-			case 0:
-				// left middle
-				break;
-
-			case 1:
-			case 2:
-			case 3:
-			case 4:
-				//middle middle
-				break;
-
-			case 5:
-				// middle right
-				break;
-
-			default:
-				return InvalidPos;
-				break;
-			}
-			break;
-
-		case 5:
-			switch (pos.second)
-			{
-			case 0:
-				// bottom left corner
-				break;
-
-			case 1:
-			case 2:
-			case 3:
-			case 4:
-				//bottomm middle
-				break;
-
-			case 5:
-				// bottom right corner
-				break;
-
-			default:
-				return InvalidPos;
-				break;
-			}
-			break;
-
-		default:
-			return InvalidPos;
-			break;
+			case 4: // middle
+				switch (pos.second)
+				{
+					case 0:  return revealBorderLeft(pos.first);
+					case 1:
+					case 2:
+					case 3:
+					case 4:  return revealMiddle(pos.first, pos.second);
+					case 5:  return revealBorderRight(pos.first);
+					default: return InvalidPos;
+				}
+			case 5: // bottom
+				switch (pos.second)
+				{
+					case 0:  return revealCornerLowerLeft();
+					case 1:
+					case 2:
+					case 3:
+					case 4:  return revealBorderBottom(pos.second);
+					case 5:  return revealCornerLowerRight();
+					default: return InvalidPos;
+				}
+			default: return InvalidPos;
 		}
+		return InvalidPos;
 	}
 
-	//apply Weg, return tries
-	int applyWeg(const Weg& w) const
+	// apply Weg, return tries
+	int applyWeg(const Weg& w)
 	{
 		int versuche = 0;
-		for (versuche = 0; versuche < w.size(); versuche++)
+		for (versuche = 0; versuche < w.size(); ++versuche)
 		{
 			std::pair<int, int> position = w.at(versuche);
-
+			int feldWert = reveal(position);
+			if (feldWert == InvalidPos)
+				return InvalidPos;
+			if (feldWert == Treasure)
+				return versuche;
 		}
+		return versuche;
 	}
 
 	~Brett() = default;
 };
 
-#endif //BRETT_H
+#endif // BRETT_H
